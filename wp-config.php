08638c5425f8773000ae01b8a11096a67522ae15
tbit8 <130091264+tbit8@users.noreply.github.com>
2023-04-08 15:16:24 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -65,6 +65,12 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
+@ini_set( 'upload_max_filesize' , '128M' );
+@ini_set( 'post_max_size', '128M');
+@ini_set( 'memory_limit', '256M' );
+@ini_set( 'max_execution_time', '300' );
+@ini_set( 'max_input_time', '300' );
+
 /**#@-*/
 
 /**
